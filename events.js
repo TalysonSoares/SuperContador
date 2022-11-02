286a4b91d74aa7d54fa29abd31c39c65bf8a0bec
TalysonSoares <talysonsoares40@gmail.com>
2022-11-02 10:56:19 -0300

    Finalizando condição para caso o usúario tenha preenchido todos os inputs corretamente

--- events.js
+++ events.js
@@ -2,8 +2,11 @@ function contar() {
     let ini = document.getElementById('txti');
     let fim = document.getElementById('txtf');
     let passo = document.getElementById('txtp');
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'contanto...'
     }
 }
