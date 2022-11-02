8dfdfdd3aa0948d0a9a5f95f6402f16c431dea81
TalysonSoares <talysonsoares40@gmail.com>
2022-11-02 11:00:10 -0300

    Convertendo os valores recebidos para número

--- events.js
+++ events.js
@@ -2,11 +2,14 @@ function contar() {
     let ini = document.getElementById('txti');
     let fim = document.getElementById('txtf');
     let passo = document.getElementById('txtp');
-    let res = document.getElementById('res')
+    let res = document.getElementById('res');
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('[ERRO] Faltam dados!')
     } else {
         res.innerHTML = 'contanto...'
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
     }
 }
