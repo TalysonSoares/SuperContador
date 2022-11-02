4babcf8ec54eff90684e37ee14d5c590b7c21b08
TalysonSoares <talysonsoares40@gmail.com>
2022-11-02 10:53:23 -0300

    Criando validação para o caso de usúario esquecer de colocar algum dado

--- events.js
+++ events.js
@@ -1,5 +1,9 @@
 function contar() {
-    let ini = document.getElementById('txt1');
-    let fim = document.getElementById('txt2');
+    let ini = document.getElementById('txti');
+    let fim = document.getElementById('txtf');
     let passo = document.getElementById('txtp');
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados!')
+    }
 }
